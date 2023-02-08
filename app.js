50d24ba880a8da93d08c33425c515697d14b8647
schen126 <86854157+schen126@users.noreply.github.com>
2023-02-08 14:27:32 -0800

    trying to format code snippet

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 console.log("Watashi ga kita!");
+const codeSnippets = [];
+codeSnippets[0] = "public static void sortForLoop(int[] arr) {/n"+
+   "    int n = arr.length;/n"+
+   "    for (int i = 1; i < n; i++) {/n"+
+   "        int key = arr[i];/n"+
+   "        int j = i - 1;/n"+
+   "        while (j >= 0 && arr[j] > key) {/n"+
+   "            arr[j + 1] = arr[j];/n"+
+   "            j--;/n"+
+   "        }/n"+
+   "        arr[j + 1] = key;/n"+
+   "    }/n"+
+   "}";
